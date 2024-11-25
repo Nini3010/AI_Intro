1eecac5fce3bf83df8eeac28b9a6aa4230ade7bf
Nini3010 <stefan-wesely@gmx.at>
2024-11-25 16:38:33 +0100

    solveable check

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AI_Intro
+
 exercises of: Introduction to AI and Data Science on FH Campus Wien
+
+# Setup
+
+pip install poetry
+
+navigate to folder:
+
+poetry install
--- ai_ue/ex1/main.py
+++ ai_ue/ex1/main.py
@@ -5,13 +5,27 @@ import numpy as np
 # create random state
 start_state = list(range(9))
 rd.shuffle(start_state)
-np.matrix(
-    start_state,
-)
+matrix = np.array(start_state).reshape(-1, 3)
+
+
+print(start_state)
+print(matrix)
+
+
 # check if state is solveable
-# inversion of row
-# inversion of column
-# unsolveable if [sum of inversions / 2] != 0
+# inversion counter
+def get_inv_count(arr: np.ndarray):
+    inv_count = 0
+    for i in range(arr.size):
+        for j in range(i + 1, arr.size):
+            if arr[j] != 0 and arr[i] != 0 and arr[i] > arr[j]:
+                inv_count += 1
+    return inv_count
+
+
+# check if puzzle is solveable via [sum of inversions mod 29 = 0
+def puzzle_solvable(matrix: np.matrix):
+    return get_inv_count(matrix.flatten()) % 2 == 0
 
 
 # heuristic 1
@@ -27,4 +41,4 @@ np.matrix(
 
 
 # compare heuristics
-print(start_state)
+print(f"solveable?: {puzzle_solvable(matrix)}")
