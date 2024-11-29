d83a4c7f97d2e84cd562cc46d5418cd11a919673
Nini3010 <stefan-wesely@gmx.at>
2024-11-29 12:11:01 +0100

    added hamming and manhattan heuristics functionality

--- ai_ue/ex1/main.py
+++ ai_ue/ex1/main.py
@@ -1,4 +1,5 @@
 import random as rd
+import time
 
 import numpy as np
 
@@ -7,14 +8,13 @@ start_state = list(range(9))
 rd.shuffle(start_state)
 matrix = np.array(start_state).reshape(-1, 3)
 
-
-print(start_state)
-print(matrix)
+# goal state
+goal_matrix = np.array(list(range(9))).reshape(-1, 3)
 
 
 # check if state is solveable
 # inversion counter
-def get_inv_count(arr: np.ndarray):
+def get_inv_count(arr: np.ndarray) -> int:
     inv_count = 0
     for i in range(arr.size):
         for j in range(i + 1, arr.size):
@@ -24,21 +24,55 @@ def get_inv_count(arr: np.ndarray):
 
 
 # check if puzzle is solveable via [sum of inversions mod 29 = 0
-def puzzle_solvable(matrix: np.matrix):
+def puzzle_solvable(matrix: np.matrix) -> bool:
     return get_inv_count(matrix.flatten()) % 2 == 0
 
 
-# heuristic 1
+# heuristic 1: Hamming distance heuristic
+# compare each number in matrix with goal matrix and if they differ add one to the hamming distance
+# we have to exclude the blank space to get the correct distance
+def get_hamming_distance(matrix: np.matrix, goal_matrix: np.matrix) -> int:
+    return sum(
+        num != goal_num and num != 0
+        for num, goal_num in zip(matrix.flatten(), goal_matrix.flatten(), strict=True)
+    )
+
 
 # time complexity
 # space complexity
 
 
-# heuristic 2
+# heuristic 2: Manhattan distance
+def get_manhattan_distance(matrix: np.matrix, goal_matrix: np.matrix) -> int:
+    distance = 0
+    for row in matrix:
+        for num in row:
+            goal_row, goal_column = np.asarray(np.where(goal_matrix == num)).flatten()
+            m_row, m_column = np.asarray(np.where(matrix == num)).flatten()
+            distance += abs(m_row - goal_row) + abs(m_column - goal_column)
+    return distance
+
 
 # time complexity
 # space complexity
+
+
+# generate successors: generate next possible moves
+def get_next_possible_moves(matrix: np.matrix):
+    next_moves = []
+    m_row, m_column = np.asarray(np.where(matrix == 0)).flatten()
+    # if row index on top border only add bottom as possible move
+    # if row index on bottom border only add top as possible move
+    # if row index in center add top and bottom as possible move
+    # same with column and left/right
 
 
 # compare heuristics
+
+print(f"start state: \n{matrix}")
+
+solveable = puzzle_solvable(matrix)
 print(f"solveable?: {puzzle_solvable(matrix)}")
+if solveable:
+    print(f"hamming distance: {get_hamming_distance(matrix,goal_matrix)}")
+    print(f"manhattan distance: {get_manhattan_distance(matrix,goal_matrix)}")
