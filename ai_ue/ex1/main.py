ae8fe8cb96332b8bb583fe6df6fb9119f7b99f80
Nini3010 <stefan-wesely@gmx.at>
2024-12-01 21:39:02 +0100

    rafactored to use Node object for path traceback; path traceback implemented;

--- ai_ue/ex1/main.py
+++ ai_ue/ex1/main.py
@@ -5,16 +5,33 @@ from math import sqrt
 
 import numpy as np
 
+# import resource
+
+
+class Node:
+    def __init__(self, parent, board, cost, step):
+        self.parent = parent
+        self.board = board
+        self.cost = cost
+        self.step = step
+        self.priority = cost + step
+
+    def __lt__(self, other):
+        return (self.board.tobytes(), self.step) < (other.board.tobytes(), other.step)
+
+    def __eq__(self, other):
+        return (self.board.tobytes(), self.step) == (other.board.tobytes(), self.step)
+
+
 # create random state
 puzzle_size = 9
 
 start_state = list(range(puzzle_size))
 rd.shuffle(start_state)
-matrix = np.array(start_state).reshape(-1, int(sqrt(puzzle_size)))
-
+START_MATRIX = np.array(start_state).reshape(-1, int(sqrt(puzzle_size)))
 
 # goal state
-GOAL_MATRIX = np.array(list(range(9))).reshape(-1, int(sqrt(puzzle_size)))
+GOAL_MATRIX = np.array(list(range(puzzle_size))).reshape(-1, int(sqrt(puzzle_size)))
 
 
 # check if state is solveable
@@ -62,94 +79,108 @@ def get_manhattan_distance(matrix: np.matrix) -> int:
 # space complexity
 
 
+def swap(matrix, row1, col1, row2, col2):
+    new_matrix = matrix.copy()
+    new_matrix[row1][col1], new_matrix[row2][col2] = (
+        new_matrix[row2][col2],
+        new_matrix[row1][col1],
+    )
+    return new_matrix
+
+
 # generate successors: generate next possible moves
-def get_next_possible_moves(matrix: np.matrix) -> list:
-    next_empty_space_coords = []
+def get_child_nodes(parent: Node, heuristic: callable) -> list[Node]:
+    parent_matrix = parent.board
     next_moves = []
-    empty_row, empty_column = np.asarray(np.where(matrix == 0)).flatten()
+    empty_row, empty_column = np.asarray(np.where(parent_matrix == 0)).flatten()
     # if row index not on top border add top as possible move
     if empty_row > 0:
-        next_empty_space_coords.append((empty_row - 1, empty_column))
+        next_moves.append(
+            swap(parent_matrix, empty_row, empty_column, empty_row - 1, empty_column)
+        )
     # if row index not on bottom border add bottom as possible move
-    if empty_row < matrix.shape[0] - 1:
-        next_empty_space_coords.append((empty_row + 1, empty_column))
+    if empty_row < parent_matrix.shape[0] - 1:
+        next_moves.append(
+            swap(parent_matrix, empty_row, empty_column, empty_row + 1, empty_column)
+        )
     # if column index not on left border add left as possible move
     if empty_column > 0:
-        next_empty_space_coords.append((empty_row, empty_column - 1))
-    # if column index not on right border add right as possible move
-    if empty_column < matrix.shape[1] - 1:
-        next_empty_space_coords.append((empty_row, empty_column + 1))
-
-    # create copy of current stage matrix and generate successor by making possible move in copy
-    for row, column in next_empty_space_coords:
-        next_matrix = matrix.copy()
-        next_matrix[empty_row][empty_column], next_matrix[row][column] = (
-            next_matrix[row][column],
-            next_matrix[empty_row][empty_column],
+        next_moves.append(
+            swap(parent_matrix, empty_row, empty_column, empty_row, empty_column - 1)
         )
-        next_moves.append(next_matrix)
-    return next_moves
+    # if column index not on right border add right as possible move
+    if empty_column < parent_matrix.shape[1] - 1:
+        next_moves.append(
+            swap(parent_matrix, empty_row, empty_column, empty_row, empty_column + 1)
+        )
+
+    child_nodes = []
+    for move in next_moves:
+        cost = calc_cost(move, heuristic)
+        child = Node(parent, move, cost, parent.step + 1)
+        child_nodes.append(child)
+    return child_nodes
 
 
 # missing: costs + current step
-def calc_costs(next_moves: list[np.matrix], heuristic: callable) -> int:
-    h_costs: list[int] = []
-    for move in next_moves:
-        h_costs.append(heuristic(move))
-    return h_costs
+def calc_cost(next_move: np.matrix, heuristic: callable) -> int:
+    return heuristic(next_move)
 
 
-def solve_puzzle(initial_state: np.matrix):
-    open_moves = []
-    visited_moves = []
-    open_moves.append(initial_state)
-    # while len(open_moves) > 0:
+def solve_puzzle(initial_state: np.matrix) -> list[Node]:
+    fastest_path: list[Node] = []
+    open_and_visited_moves_bytes = []
+    queue: list[(int, Node)] = []
+
+    move = initial_state
+    step = 0
+    cost = calc_cost(move, heuristic)
+    priority = cost
+    node = Node(None, move, cost, step)
+    heapq.heappush(queue, (priority, node))
+
+    while queue:
+        heap_op = heapq.heappop(queue)
+        priority: int = heap_op[0]
+        node: Node = heap_op[1]
+
+        # visited_moves.append((priority, cost, step, move))
+        open_and_visited_moves_bytes.append(node.board.tobytes())
+        if np.array_equal(node.board, GOAL_MATRIX):
+            t_node = node
+            while t_node.parent is not None:
+                fastest_path.append(t_node)
+                t_node = t_node.parent
+            fastest_path.reverse()
+            return fastest_path
+
+        child_nodes = get_child_nodes(node, heuristic)
+
+        for node in child_nodes:
+            b_move = node.board.tobytes()
+            if b_move not in open_and_visited_moves_bytes:
+                open_and_visited_moves_bytes.append(b_move)
+                heapq.heappush(queue, (node.priority, node))
+    return fastest_path
 
 
 # compare heuristics
-solveable = False
-while not solveable:
-    print(f"start state: \n{matrix}")
-    solveable = puzzle_solvable(matrix)
-    print(f"solveable?: {puzzle_solvable(matrix)}")
+print(f"start state: \n{START_MATRIX}")
+solveable = puzzle_solvable(START_MATRIX)
+print(f"solveable?: {solveable}")
 if solveable:
-    print(f"heuristic?: \n1: Hamming\n2: Manhattan\n9: Benchmark\n(input number)")
+    print("heuristic?: \n1: Hamming\n2: Manhattan\n9: Benchmark\n(input number)")
     heuristic_num = int(input())
     if heuristic_num == 1:
         heuristic = get_hamming_distance
     elif heuristic_num == 2:
         heuristic = get_manhattan_distance
     if heuristic is not None:
-        unsolved = True
-        visited_moves = []
-        queue = []
+        solve_path = solve_puzzle(START_MATRIX)
+        print(f"solved in {len(solve_path)} steps")
+        for node in solve_path:
+            print(f"step: {node.step}\n{node.board}\n")
 
-        move = matrix
-        step = 0
-        cost = calc_costs([matrix], heuristic)[0]
-        priority = cost
-        visited_moves.append((priority, cost, step, matrix))
-        heapq.heappush(queue, (priority, cost, step, 0, move))
 
-        while len(queue) > 0:
-            priority, cost, step, _, move = heapq.heappop(queue)
-            visited_moves.append((priority, cost, step, matrix))
-            if np.array_equal(move, GOAL_MATRIX):
-                break
-
-            moves = get_next_possible_moves(move)
-            step += 1
-            costs = calc_costs(moves, heuristic)
-            priorities = [x + step for x in costs]
-
-            for priority, cost, move in zip(priorities, costs, moves, strict=True):
-                salt = rd.randbytes(2)
-                heapq.heappush(queue, (priority, cost, step, salt, move))
-
-            # prio, step, _, cost, move = heapq.heappop(queue)
-            # visited_moves.append((prio, step, cost, move))
-            # for prio,move in zip(priorities,moves):
-
-            # print(f"hamming distance: {get_hamming_distance(matrix,GOAL_MATRIX)}")
-            # print(f"manhattan distance: {get_manhattan_distance(matrix,GOAL_MATRIX)}")
-            # print(f"successors stage 1: {get_next_possible_moves(matrix)}")
+# ram usage
+# mem_init = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
